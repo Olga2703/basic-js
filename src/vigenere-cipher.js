904d2591a2eb75573e2743b026ad98089a2cfef9
Olga <smirnova_olga18@mail.ru>
2021-03-15 18:21:58 +0300

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(reverse) {
+    this.reverse = reverse;
+  }
+  encrypt(message, key) {
+    let result = new String();
+    let eng = new Array();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 65; i < 91; i++) {
+      eng.push(String.fromCharCode(i));
+    }
+    for (let i = 0, k = 0; i < message.length; i++, k++) {
+      if (eng.includes(message[i])) {
+        let index = (eng.indexOf(message[i]) + eng.indexOf(key.charAt(k % key.length))) % eng.length;
+        result += eng[index];
+      } else {
+        k = k - 1;
+        result += message[i];
+      }
+    }
+    return (this.reverse === false) ? result.split('').reverse().join('') : result;
+  }
+
+  decrypt(message, key) {
+    let result = new String();
+    let eng = new Array();
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    for (let i = 65; i < 91; i++) {
+      eng.push(String.fromCharCode(i));
+    }
+
+    for (let i = 0, k = 0; i < message.length; i++, k++) {
+      if (eng.includes(message[i])) {
+        let index = ((eng.indexOf(message[i]) + eng.length) - eng.indexOf(key.charAt(k % key.length))) % eng.length;
+        result += eng[index];
+      } else {
+        k = k - 1;
+        result += message[i];
+      }
+    }
+    return (this.reverse === false) ? result.split('').reverse().join('') : result;
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
